e57db7791fc8ff811ff1c54c4b53749f8ef805eb
soma matsumoto <souma67jiji@icloud.com>
2025-09-29 21:28:31 +0900

    ヘッダー部分作成

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function add_files() {
+    wp_enqueue_style('reset-style', 'https://unpkg.com/destyle.css@2.0.2/destyle.css');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap');
+    wp_enqueue_style('main-style', get_stylesheet_uri());
+}
+
+function theme_setup() {
+    add_theme_support('title-tag');
+}
+
+add_action('wp_enqueue_scripts', 'add_files');
+add_action('after_setup_theme', 'theme_setup');
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo esc_url(get_theme_file_uri('/images/leaf-left.svg'));?>" type="image/svg+xml">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <header class="site-header">
+        <div class="wrapper">
+            <h1 class="site-title">
+                <a href="<?php echo esc_url(home_url('/')); ?>">
+                    <img class="site-logo" src="<?php echo esc_url(get_theme_file_uri('/images/logo.svg')); ?>" alt="<?php bloginfo('name'); ?>">
+                </a>
+            </h1>
+            <p class="site-description"><?php bloginfo('description'); ?></p>
+        </div>
+    </header>
+    <nav class="primary-navigation wrapper">
+        <button class="btn-menu">メニュー</button>
+        <ul class="menu-wrapper">
+            <li><a href="#">ホーム</a></li>
+            <li><a href="#">オーガニックフード</a></li>
+            <li><a href="#">インテリア</a></li>
+            <li><a href="#">アウトドアグッズ</a></li>
+            <li><a href="#">ヒーリング</a></li>
+        </ul>
+    </nav>
